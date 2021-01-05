7c0d866b909bc52b3ad8365a5a220b4a18de26d3
CodeMari <amaril@uci.edu>
2021-01-04 22:28:01 -0800

    Initial Commit

---
+++ Flix_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix_app
+//
+//  Created by Amari Lewis on 04/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
